beb2781cc81eff08edaac6fea4ddf8d097fab988
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-08 10:27:16 +0100

    add suport for drop

--- sktransf/__init__.py
+++ sktransf/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __doc__ = """
 Basic package to enable usefull transformers in scikit-learn pipelines.
@@ -7,9 +7,12 @@ First transformer implemented is a LogTransformer, which is a simple wrapper aro
 """
 
 from .logger import LogTransformer
+from .drop import DropUniqueColumnTransformer, BoolColumnTransformer
 from ._utils import get_titanic, use_case
 
 __all__ = [
+    "DropUniqueColumnTransformer",
+    "BoolColumnTransformer",
     "LogTransformer",
     "get_titanic",
     "use_case",
