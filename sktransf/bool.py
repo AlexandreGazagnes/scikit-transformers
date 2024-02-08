1f3884ff43590461f36cfbfdf07403397fb3ecba
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-08 12:07:03 +0100

    fix typing

--- sktransf/bool.py
+++ sktransf/bool.py
@@ -2,6 +2,7 @@
 Boolean column transformer
 """
 
+
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
--- sktransf/drop_unique.py
+++ sktransf/drop_unique.py
@@ -2,6 +2,7 @@
 Unique drop transformer
 """
 
+
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
